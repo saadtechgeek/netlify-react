69d6dc676a882ee3bad134a1472e573f51ea6cb9
Saad Shahid <saad.techgeek@gmail.com>
2020-10-15 19:20:38 +0500

    resolve lint errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <h1>Hello world - React App</h1>
+      <h1>Hello world - React App - updated</h1>
     </div>
   );
 }
 
+export default App;
